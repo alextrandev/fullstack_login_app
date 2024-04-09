7c30ffd6764c35070281b22f1a857180fbb4b6ed
dk10119 <tranduclong092@gmail.com>
2024-04-09 12:12:17 +0300

    link 2 components to the App, create a form in the CreateUser component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
-import CreateUser from './components/CreateUser'
-import ListUser from './components/ListUser'
+import {BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom';
+import ListUser from './components/ListUser';
+import CreateUser from './components/CreateUser';
+import './App.css';
 
 function App() {
   return (
     <>
-      <CreateUser />
-      <ListUser />
+      <h1>Fullstack App</h1>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">List Users</NavLink>
+            </li>
+            <li>
+              <NavLink to="user/create">Create Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path='user/create' element={<CreateUser />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,6 +1,17 @@
 function CreateUser() {
   return (
-    <div>CreateUser</div>
+    <>
+    <h2>CreateUser</h2>
+    <form className="flex flex-col">
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+      <label htmlFor="email">E-mail</label>
+      <input type="email" name="email" id="email" />
+      <label htmlFor="phone">Phone number</label>
+      <input type="number" name="phone" id="phone" />
+      <input type="submit" value="Submit" />
+    </form>
+    </>
   )
 }
 
